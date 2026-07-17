65c55ca5fe206043c0b50747011a5e29972f29cd
LionnoiL <wmconstructor@gmail.com>
2026-07-17 15:26:55 +0300

    Implement game status management and update next piece functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 import * as t from "./tetrominoes.js";
+import { GAME_STATUS } from "./utils.js";
 
 const CELL_SIZE = 40;
+
+const startBtnEl = document.querySelector(".button-start");
+const nextPieceEl = document.querySelector(".next-piece");
+
+let mainLoopTimer;
+let gameStatus = GAME_STATUS.stopped;
+let nextFigure = t.getRandomTetromino();
+let currentFigure = null;
+const playground = [];
 
 function createPlayground() {
   const playgroundDocument = document.querySelector(".playground");
@@ -8,8 +18,10 @@ function createPlayground() {
 
   for (let i = 0; i < 20; i++) {
     playgroundText += `<div class="row">`;
+    playground[i] = [];
     for (let j = 0; j < 10; j++) {
       playgroundText += `<svg width="${CELL_SIZE}" height="${CELL_SIZE}"><use href="./images/sprites.svg#icon-grid-unit"></use></svg>`;
+      playground[i][j] = null;
     }
     playgroundText += `</div>`;
   }
@@ -45,17 +57,39 @@ function placeNext(tetromino) {
   });
 }
 
+function newGameStatus(newStatus) {
+  gameStatus = newStatus;
+  if (gameStatus === GAME_STATUS.running) {
+    startBtnEl.textContent = "Pause";
+    mainLoopTimer = window.setInterval(loop, 500);
+  } else {
+    startBtnEl.textContent = "Play";
+    clearInterval(mainLoopTimer);
+  }
+}
+
 const startGame = () => {
-  console.log("start");
+  if (gameStatus === GAME_STATUS.running) {
+    newGameStatus(GAME_STATUS.paused);
+    return;
+  } else {
+    newGameStatus(GAME_STATUS.running);
+  }
+
+  currentFigure = nextFigure;
+  generateNewTetromino();
 };
 
-const startBtnEl = document.querySelector(".button-start");
+const generateNewTetromino = () => {
+  nextFigure = t.getRandomTetromino();
+  renderTetromino(nextFigure);
+  placeNext(nextFigure);
+};
+
+const loop = () => {};
+
 startBtnEl.addEventListener("click", startGame);
+nextPieceEl.addEventListener("click", generateNewTetromino);
 
+clearInterval(mainLoopTimer);
 createPlayground();
-
-const nextFigure = t.getRandomTetromino();
-
-renderTetromino(nextFigure);
-
-placeNext(nextFigure);
---
+++ js/utils.js
@@ -0,0 +1,5 @@
+export const GAME_STATUS = {
+  stopped: "stopped",
+  running: "running",
+  paused: "paused",
+};
